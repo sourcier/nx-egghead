96d618018f6f480a92072b4aa16546219f9956e8
Roger Rajaratnam <roger@sourcier.uk>
2022-02-01 22:31:45 +0000

    chore: Connect to Express API from React app

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import './app.scss';
+
+import { Header } from '@nx-egghead/store/ui-shared';
 
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -6,64 +10,94 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-import './app.scss';
-import { getAllGames } from '../fake-api';
-import { Header } from '@nx-egghead/store/ui-shared';
 import { formatRating } from '@nx-egghead/store/util-formatters';
 
 import { Route, useHistory } from 'react-router-dom';
 
 import { StoreFeatureGameDetail } from '@nx-egghead/store/feature-game-detail';
 
-export function App() {
+export const App = () => {
   const history = useHistory();
+  const [state, setState] = useState<{
+    data: any[];
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: [],
+    loadingState: 'success',
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+    fetch('/api/games')
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, []);
 
   return (
     <>
       <Header />
       <div className="container">
         <div className="games-layout">
-          {getAllGames().map((x) => (
-            <Card
-              key={x.id}
-              className="game-card"
-              onClick={() => history.push(`/game/${x.id}`)}
-            >
-              <CardActionArea>
-                <CardMedia
-                  className="game-card-media"
-                  image={x.image}
-                  title={x.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {x.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    className="game-rating"
-                  >
-                    <strong>Rating:</strong> {formatRating(x.rating)}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
+          {state.loadingState === 'loading'
+            ? 'Loading...'
+            : state.loadingState === 'error'
+            ? '<div>Error retrieving data</div>'
+            : state.data.map((x) => (
+                <Card
+                  key={x.id}
+                  className="game-card"
+                  onClick={() => history.push(`/game/${x.id}`)}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      className="game-card-media"
+                      image={x.image}
+                      title={x.name}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {x.name}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {x.description}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                        className="game-rating"
+                      >
+                        <strong>Rating:</strong> {formatRating(x.rating)}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
         </div>
       </div>
 
       <Route path="/game/:id" component={StoreFeatureGameDetail} />
     </>
   );
-}
+};
 
 export default App;
