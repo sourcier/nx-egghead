525818afd78aad0421f395d40d8d71e4696c9b60
Roger Rajaratnam <roger@sourcier.uk>
2022-02-01 01:04:20 +0000

    chore: Install and use external packages

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,52 +1,48 @@
+import React from 'react';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
-import { Route, Link } from 'react-router-dom';
+import { getAllGames } from '../fake-api';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="store" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
+    <div className="container">
+      <div className="games-layout">
+        {getAllGames().map((x) => (
+          <Card key={x.id} className="game-card">
+            <CardActionArea>
+              <CardMedia
+                className="game-card-media"
+                image={x.image}
+                title={x.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {x.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {x.description}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  className="game-rating"
+                >
+                  <strong>Rating:</strong> {x.rating}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
       </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
-    </>
+    </div>
   );
 }
 
